39b7f7a7c5d15449dced35a8f98f6048700783ac
Allan Leal <allanmulin@gmail.com>
2018-04-17 08:28:38 +0200

    Added tests for fix of issue #1258

--- tests/test_stl.py
+++ tests/test_stl.py
@@ -198,3 +198,10 @@ def test_missing_header_message():
     with pytest.raises(TypeError) as excinfo:
         cm.missing_header_return()
     assert expected_message in str(excinfo.value)
+
+
+def test_function_with_string_and_vector_string_arg():
+    """Check if a string is NOT implicitly converted to a list, which was the
+    behavior before fix of issue #1258"""
+    assert m.func_with_string_or_vector_string_arg_overload(['A', 'B']) == 0
+    assert m.func_with_string_or_vector_string_arg_overload('A') == 1
